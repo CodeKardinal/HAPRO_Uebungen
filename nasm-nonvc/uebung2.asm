5e40cdb65949288519e38a86ec0d1d2c5a329110
Vincent Maik Becker <s3004171@ba-sachsen.de>
2022-08-29 18:31:51 +0200

    penis

--- nasm-nonvc/uebung2.asm
+++ nasm-nonvc/uebung2.asm
@@ -5,23 +5,33 @@ bits 64
 %include "win32n.inc"
 
 global Start
-;test
+
 section .text
 Start:
     ; 1. Bits 7…4 von EAX sollen in die Bits 3…0 übertragen und negiert werden
     xor rax, rax    ;rax=0
     mov al, 160     ;             al=10100000
+    mov ah, al      ;             ah=10100000
     shr al, 4       ;rechts-shift al=00001010
-    xor al, 255     ;negation     al=11110101
+    or al, 240      ;             al=11111010
+    not al          ;negation     al=00000101
+    xor al, ah      ;             al=10100101
+
 
     ; 2. …in umgekehrter Reihenfolge
     xor rax, rax    ;rax=0
-    mov al, 160     ;            al=10100000
-    shl al, 4       ;links-shift al=00000000
-    xor al, 255     ;negation    al=11111111
+    mov al, 10     ;            al=00001010 01010000
+    
+    mov rbx, 8
 
+    jump:
+    Dec rbx
+    rcr al, 1
+    rcl ah, 1
+    jnz jump
+ 
     ; 3. Multiplikation von A123h mit BX=7 
-    xor rax, rax
+    ;xor rax, rax
 
 
 
